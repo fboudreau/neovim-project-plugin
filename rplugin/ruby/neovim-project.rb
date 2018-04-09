047d8671ce0c4184e6a37c7970f9f908b5f9d07c
Fernand Boudreau <fernand.boudreau@gmail.com>
2018-04-08 23:16:43 -0400

    Added search from NERDTree node

--- rplugin/ruby/neovim-project.rb
+++ rplugin/ruby/neovim-project.rb
@@ -24,8 +24,10 @@ Neovim.plugin do |plug|
     # ProjectOpen.
     plug.autocmd(:VimEnter) do |nvim|
       nvim.command("map <leader>ap :ProjectOpen<CR>")
+      #nvim.command("autocmd BufReadPost,FileReadPost * :ProjectAddPath")
+      #nvim.command("autocmd BufDelete * :ProjectRemovePath")
+      nvim.command("autocmd VimLeave * :mksession! ~/bob")
     end
-
 
     plug.command(:ProjectNew, :nargs => '*', :complete => :file) do |nvim, name, path|
 
@@ -40,6 +42,7 @@ Neovim.plugin do |plug|
                     proj = {
                         "version" => "1.0.0",
                         "name" => name, 
+                        "root" => nil,
                         "paths" => [], 
                         "open_browsers" => true,
                         "ack_options" => "--ignore-file=is:tags --ignore-file=ext:map --ignore-file=ext:d"
@@ -66,11 +69,19 @@ Neovim.plugin do |plug|
         end
     end
 
+    plug.command(:ProjectMessage, :nargs => 1) do |nvim, string|
+         nvim.message(string + "\n")
+    end
+
     # This command will search files using Ack from the root of the project.
     plug.command(:ProjectAck, :nargs => 1) do |nvim, string|
 
-        # Run Ack without jumping to the first entry..
-        nvim.command("Ack! #{proj['ack_options']} #{string} #{proj[:root]}")
+        if not proj.nil?
+            # Run Ack without jumping to the first entry..
+            nvim.command("Ack! #{proj['ack_options']} #{string} #{proj["root"]}")
+        else
+            nvim.message("Project not open. Please use :ProjectOpen.\n")
+        end
 
     end
 
@@ -78,30 +89,75 @@ Neovim.plugin do |plug|
     # This comman will search files using Ack from the selected location in the NERDtree
     plug.command(:ProjectAckFrom, :nargs => 1) do |nvim, string|
 
-        # The user has to be in the NERDtree buffer to use this command.
-        # This command needs to know which node is selected in the tree.
-        if nvim.get_current_buf.name =~ /NERD_tree_[0-9]+/
+        if not proj.nil?
+            # The user has to be in the NERDtree buffer to use this command.
+            # This command needs to know which node is selected in the tree.
+            if nvim.get_current_buf.name =~ /NERD_tree_[0-9]+/
 
-            selected_path = nvim.evaluate('g:NERDTreeFileNode.GetSelected().path.str()')
+                selected_path = nvim.evaluate('g:NERDTreeFileNode.GetSelected().path.str()')
 
-            # if a file is selected, we will search from it's parent directory. Otherwise,
-            # we will search from the selected directory
-            if File.file?(selected_path)
-                @path = Pathname(selected_path).parent.to_s
+                # if a file is selected, we will search from it's parent directory. Otherwise,
+                # we will search from the selected directory
+                if File.file?(selected_path)
+                    @path = Pathname(selected_path).parent.to_s
+                else
+                    @path = Pathname(selected_path).to_s
+                end
+
+                # Run Ack without jumping to the first entry. That's what the bang (!) is for. 
+                nvim.command("Ack! #{proj['ack_options']} #{string}  #{@path}")
+
             else
-                @path = Pathname(selected_path).to_s
+                # Most unfortunate, this. Can't do things like ctrl-r ctrl-w, for example. 
+                # TODO: Is there a away to get at the selected node from another buffer?
+                nvim.message("You must select a path in the file and directory explorer before using this command.\n")
             end
 
-            # Run Ack without jumping to the first entry. That's what the bang (!) is for. 
-            nvim.command("Ack! #{proj['ack_options']} #{string}  #{@path}")
-
         else
-            # Most unfortunate, this. Can't do things like ctrl-r ctrl-w, for example. 
-            # TODO: Is there a away to get at the selected node from another buffer?
-            nvim.message("You must select a path in the file and directory explorer before using this command.\n")
+            nvim.message("Project not open. Please use :ProjectOpen.\n")
         end
     end
 
+    plug.command(:ProjectFind, :complete => :file, :nargs => '*') do |nvim, path|
+
+        if not proj.nil?
+            nvim.command("CommandT #{proj["root"]}")
+        else
+            nvim.message("Project not open. Please use :ProjectOpen.\n")
+        end
+    end
+
+    plug.command(:ProjectFindFrom, :complete => :file, :nargs => '*') do |nvim, path|
+
+        if not proj.nil?
+            # The user has to be in the NERDtree buffer to use this command.
+            # This command needs to know which node is selected in the tree.
+            if nvim.get_current_buf.name =~ /NERD_tree_[0-9]+/
+
+                selected_path = nvim.evaluate('g:NERDTreeFileNode.GetSelected().path.str()')
+
+                # if a file is selected, we will search from it's parent directory. Otherwise,
+                # we will search from the selected directory
+                if File.file?(selected_path)
+                    @path = Pathname(selected_path).parent.to_s
+                else
+                    @path = Pathname(selected_path).to_s
+                end
+
+                # Run Ack without jumping to the first entry. That's what the bang (!) is for. 
+                nvim.command("CommandT #{@path}")
+
+            else
+                # Most unfortunate, this. Can't do things like ctrl-r ctrl-w, for example. 
+                # TODO: Is there a away to get at the selected node from another buffer?
+                nvim.message("You must select a path in the file and directory explorer before using this command.\n")
+            end
+        else
+            nvim.message("Project not open. Please use :ProjectOpen.\n")
+        end
+
+
+    end
     #Open an existing project
     plug.command(:ProjectOpen, :complete => :file, :nargs => '*') do |nvim, path|
        
@@ -120,26 +176,41 @@ Neovim.plugin do |plug|
 
                 # Store the absolute path to the project. Doesn't need to be in the file since we can
                 # figure it out based on it's location.
-                proj[:root] = File.absolute_path(path)
+                proj["root"] = File.absolute_path(path)
 
                 # Add some maps
                 nvim.command("map <c-n> :ProjectShowExplorer<CR>")
                 nvim.command("map <leader>aa :ProjectAck ")
                 nvim.command("map <leader>af :ProjectAckFrom ")
                 nvim.command("map <leader>at :ProjectGenerateTags<CR>")
+                nvim.command("map <leader>ac :ProjectFind<CR>")
 
                 # Git related
                 nvim.command("map <leader>gk :silent !gitk --all<CR>")
                 nvim.command("map <leader>gg :silent !git gui<CR>")
+                nvim.command("set sessionoptions=buffers")
 
                 # Automatically show the explorers?
                 
                 if proj["open_browsers"] == true
                     nvim.command(":ProjectShowExplorer")
+                    nvim.command(":silent TlistClose")
                     nvim.command(":Tlist")
                 end
 
                 logger.info("Project file loaded: " + proj.inspect)
+
+
+                # Check if user oppened any files from the command line. If so
+                # add them to the paths list
+                #nvim.list_bufs.each{|buffer|
+                #    nvim.command("ProjectAddPath")
+                #}
+
+                 
+                #proj['paths'].each{|p|
+                #    nvim.command(":badd " + p)
+                #}
 
             else
                 nvim.message("Project file not found.\n")
@@ -151,17 +222,63 @@ Neovim.plugin do |plug|
     end
 
     plug.command(:ProjectGenerateTags, :nargs => 0){|nvim|
+        
         if not proj.nil?
             logger.info("Generating tags list")
-            logger.info("Root path: #{proj[:root]}")
-            system("find #{proj[:root]} -type f | ctags -f #{proj[:root]}/tags -L -")
+            logger.info("Root path: #{proj["root"]}")
+            system("find #{proj["root"]} -type f | ctags -f #{proj["root"]}/tags -L -")
         else
             nvim.message("Project not open. Please use :ProjectOpen.\n")
         end
     }
 
-    plug.command(:ProjectAddPath, :nargs => 1, :compilete => :file){|nvim, path|
+
+
+    plug.command(:ProjectRemovePath, :nargs => '?', :complete => :file){|nvim, path|
+
+        if not proj.nil?
+
+            if path.nil?
+                path = File.absolute_path(nvim.get_current_buf.name)
+            else
+                path = File.absolute_path(path)
+            end
+
+            proj['paths'].delete(path)
+
+            File.open(proj["root"] + File::SEPARATOR + PROJECT_FILE_NAME, "w" ){|f|
+                f.write(proj.to_yaml)
+            }
+
+        else
+            nvim.message("Project not open. Please use :ProjectOpen.\n")
+        end
+
+
+    }
+
+    plug.command(:ProjectAddPath, :nargs => '?', :complete => :file){|nvim, path|
+
         
+        if not proj.nil?
+            
+            if path.nil?
+                path = File.absolute_path(nvim.get_current_buf.name)
+            else
+                path = File.absolute_path(path)
+            end
+
+            proj['paths'] << path if not proj['paths'].include?(path)
+
+            File.open(proj["root"] + File::SEPARATOR + PROJECT_FILE_NAME, "w" ){|f|
+                f.write(proj.to_yaml)
+            }
+
+        else
+            nvim.message("Project not open. Please use :ProjectOpen.\n")
+        end
+
+
     }
 
     # List paths that are part of the project
@@ -183,7 +300,8 @@ Neovim.plugin do |plug|
 
     plug.command(:ProjectShowExplorer, nargs: 0) do |nvim|
         if not proj.nil?
-            nvim.command(":NERDTree #{proj[:root]}")
+            nvim.command(":NERDTreeClose")
+            nvim.command(":NERDTree #{proj["root"]}")
         else
             nvim.message("Project not open. Please use :ProjectOpen.\n")
         end
@@ -193,12 +311,23 @@ Neovim.plugin do |plug|
     plug.function(:ProjectGetRootPath, nargs: 0, sync: true) do |nvim|
 
         if not proj.nil?
-            proj[:root]
+            proj["root"]
         else
             nvim.message("Project not open. Please use :ProjectOpen.\n")
         end
 
     end
+
+    plug.function(:ProjectAddPath, nargs: 0, sync: true) do |nvim|
+        if not proj.nil?
+            nvim.message(nvim.get_current_buf.name + "\n")
+        else
+            nvim.message("Project not open. Please use :ProjectOpen.\n")
+        end
+
+    end
+
+
 
     private
 
